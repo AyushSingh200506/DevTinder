347b29608fb638d3edee336841c39ea86cef13c9
AyushSingh200506 <ayushsingh200506@gmail.com>
2025-12-28 10:05:56 +0530

    Exploring GET, POST and DELETE API calls.

--- src/app.js
+++ src/app.js
@@ -2,15 +2,18 @@ import express from 'express';
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-  res.send('Hello, DevTinder! from test route');
+app.get("/user", (req, res) => {
+  res.send('Hello, DevTinder! from user route');
 });
 
-app.use("/",(req, res) => {
-  res.send('Hello, DevTinder! from root');
+app.post("/user", (req, res) => {
+  res.send('User created!');
+});
+
+app.delete("/user", (req, res) => {
+  res.send('User deleted!');
 });
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
-
