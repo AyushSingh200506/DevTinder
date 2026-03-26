73ea10f1246024a1acf16fea5e1750bea03adf46
AyushSingh200506 <ayushsingh200506@gmail.com>
2026-03-26 12:38:11 +0530

    feat: Add default photoURL field to user schema

--- src/models/user.js
+++ src/models/user.js
@@ -52,6 +52,10 @@ const userSchema = new mongoose.Schema({
             message: "Gender must be Male, Female, or Other"
         },
     },
+    photoURL: {
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
     skills: {
         type: [String],
     }
